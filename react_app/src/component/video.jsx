0460a4822a5835f5f357816a570bb3e2fb5b3b3a
Ktakahiro <19238182@edu.cc.saga-u.ac.jp>
2023-07-08 22:45:47 +0900

    update video.jsx

--- react_app/src/component/video.jsx
+++ react_app/src/component/video.jsx
@@ -1,4 +1,5 @@
 import { React, useState, useEffect } from "react";
+import AWS from 'aws-sdk';
 
 export const VideoComponent = () => {
   let width = (window.innerWidth / 3) * 2; //get the window width
@@ -26,7 +27,7 @@ export const VideoComponent = () => {
       "canplay",
       (ev) => {
         if (!streaming) {
-          width = window.innerWidth; //get window width
+          width = window.innerWidth/2; //get window width
           height = video.videoHeight / (video.videoWidth / width);
           video.setAttribute("width", width);
           video.setAttribute("height", height);
@@ -77,14 +78,70 @@ export const VideoComponent = () => {
       canvas.width = width;
       canvas.height = height;
       context.drawImage(video, 0, 0, width, height);
-
+      
       const data = canvas.toDataURL("image/png");
+      let bin = atob(data.replace(/^.*,/, '')); // (1)ファイルをバイナリ化
+      let buffer = new Uint8Array(bin.length); // (2)バイナリデータに変換する
+      for (let i = 0; i < bin.length; i++) {
+          buffer[i] = bin.charCodeAt(i);
+      } 
+      // (3)Fileオブジェクトを生成
+      let image_file = new File([buffer.buffer], randomFileName(), {type: 'image/png'});
+      addPhoto(image_file);
+
+      console.log(data)
       photo.setAttribute("src", data);
       document.getElementById("canvas").style.display = "none";
     } else {
       clearPhoto();
     }
   }
+
+  function addPhoto(file) {
+    var albumBucketName = "kokushimusou";
+    var bucketRegion = "ap-northeast-1";
+    var IdentityPoolId = "ap-northeast-1:27df68ca-3e55-4ff2-8ad5-01216bfbb9c6";
+
+    AWS.config.update({
+      region: bucketRegion,
+      credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId
+      })
+    });
+
+    var s3 = new AWS.S3({
+      apiVersion: "2006-03-01",
+      params: { Bucket: albumBucketName }
+    });
+    var fileName = file.name;
+    var albumPhotosKey = encodeURIComponent('GroupPicture') + "/";
+  
+    var photoKey = albumPhotosKey + fileName;
+  
+    // Use S3 ManagedUpload class as it supports multipart uploads
+    var upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: albumBucketName,
+        Key: photoKey,
+        Body: file
+      }
+    });
+    var promise = upload.promise();
+    promise.then(
+      function(data) {
+        alert("Successfully uploaded photo.");
+        console.log(data);
+      },
+      function(err) {
+        return alert("There was an error uploading your photo: ", err.message);
+      }
+    );
+  }
+
+  function randomFileName() {
+    return Math.random().toString(32).substring(2);
+  }
+  
 
   function isSmartPhone() {
     if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
@@ -128,7 +185,7 @@ export const VideoComponent = () => {
             Take
             picture
           >
-            take picture
+            Upload Picture
           </button>
           <div className="my-2">
             state：<span id="status"></span>---device：<span id="device"></span>
